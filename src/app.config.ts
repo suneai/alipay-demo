6f002e74614f673b2340701cb7d4e5472fe4f660
sun.xiaoye <sun.xiaoye@mwee.cn>
2021-06-08 13:41:47 +0800

    update

---
+++ src/app.config.ts
@@ -0,0 +1,19 @@
+/*
+ * @Description: 
+ * @Author: sunxiaoye
+ * @Date: 2021-06-08 11:22:34
+ * @LastEditTime: 2021-06-08 11:32:35
+ * @LastEditors: sunxiaoye
+ */
+export default {
+  pages: [
+    'pages/index/index',
+    'pages/index/native/native'
+  ],
+  window: {
+    backgroundTextStyle: 'light',
+    navigationBarBackgroundColor: '#fff',
+    navigationBarTitleText: 'WeChat',
+    navigationBarTextStyle: 'black'
+  }
+}
---
+++ src/pages/index/native/native.js
@@ -0,0 +1,14 @@
+/*
+ * @Description: 
+ * @Author: sunxiaoye
+ * @Date: 2021-06-08 11:31:06
+ * @LastEditTime: 2021-06-08 11:32:13
+ * @LastEditors: sunxiaoye
+ */
+
+// eslint-disable-next-line no-undef
+Page({
+    onLoad(){
+
+    }
+})
